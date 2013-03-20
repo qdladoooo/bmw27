efc9271ab5231cf53a499d24f2565312c4f10174
Dlad <dlad@wobu2.com>
2013-03-20 11:18:47 +0800

    push to github

--- data/smarty/compile/d15adafac29fecec779c63cf643eec7933eabf9a.file.layout.html.php
+++ data/smarty/compile/d15adafac29fecec779c63cf643eec7933eabf9a.file.layout.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2013-03-18 22:29:42
+<?php /* Smarty version Smarty-3.1.13, created on 2013-03-20 11:06:44
          compiled from "templates/layout/layout.html" */ ?>
 <?php /*%%SmartyHeaderCode:15272482015146ad0e237605-65839163%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'd15adafac29fecec779c63cf643eec7933eabf9a' => 
     array (
       0 => 'templates/layout/layout.html',
-      1 => 1363597075,
+      1 => 1363748667,
       2 => 'file',
     ),
   ),
@@ -19,6 +19,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'unifunc' => 'content_5146ad0e25a681_26684723',
   'variables' => 
   array (
+    'title' => 0,
     'content' => 0,
   ),
   'has_nocache_code' => false,
@@ -27,23 +28,36 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <html lang="zh-CN">
     <head>
 		<meta charset="utf-8">
-		<title></title>        
+		<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Le styles -->
-		<link href="/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
-		<link href="/css/global.css" media="screen" rel="stylesheet" type="text/css">
-		<link href="/css/style.css" media="screen" rel="stylesheet" type="text/css">
-		<link href="/css/bootstrap-responsive.min.css" media="screen" rel="stylesheet" type="text/css">
-		<link href="/images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+		<link href="<?php echo @constant('__CSS_PATH');?>
+bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
+		<link href="<?php echo @constant('__CSS_PATH');?>
+global.css" media="screen" rel="stylesheet" type="text/css">
+		<link href="<?php echo @constant('__CSS_PATH');?>
+bootstrap-responsive.min.css" media="screen" rel="stylesheet" type="text/css">
+		<link href="<?php echo @constant('__IMG_PATH');?>
+favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
 		<!-- Scripts -->
-		<script type="text/javascript" src="/js/jquery.min.js"></script>
-		<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?php echo @constant('__JS_PATH');?>
+jquery.min.js"></script>
+		<script type="text/javascript" src="<?php echo @constant('__JS_PATH');?>
+bootstrap.min.js"></script>
 		<!--[if lt IE 9]><script type="text/javascript" src="/js/html5.js"></script><![endif]-->                                      
 	</head>
 <body>
+<div id="stage">
+	<div id="header" class="row">
+		<div class="span4">...</div>
+	  	<div class="span8">...</div>
+	</div>
+	
+	<?php echo $_smarty_tpl->tpl_vars['content']->value;?>
 
-<?php echo $_smarty_tpl->tpl_vars['content']->value;?>
 
+</div>
 
 </body>
 </html><?php }} ?>
--- data/smarty/compile/ffad99e947070e0d6d68e44dde7b6206d426c225.file.test.html.php
+++ data/smarty/compile/ffad99e947070e0d6d68e44dde7b6206d426c225.file.test.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2013-03-18 11:13:43
+<?php /* Smarty version Smarty-3.1.13, created on 2013-03-20 10:09:52
          compiled from "templates/test/test.html" */ ?>
 <?php /*%%SmartyHeaderCode:42106513051468667006810-05768873%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'ffad99e947070e0d6d68e44dde7b6206d426c225' => 
     array (
       0 => 'templates/test/test.html',
-      1 => 1363576045,
+      1 => 1363745389,
       2 => 'file',
     ),
   ),
@@ -15,16 +15,38 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_5146866702c346_79449067',
   'variables' => 
   array (
     'name' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.13',
-  'unifunc' => 'content_5146866702c346_79449067',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_5146866702c346_79449067')) {function content_5146866702c346_79449067($_smarty_tpl) {?><?php echo $_smarty_tpl->tpl_vars['name']->value;?>
 
 
 mememememe
+
+
+<button id="fat-btn" data-loading-text="loading..." class="btn btn-primary">载入状态</button>
+              
+              
+<div class="accordion-group" style="width:400px">
+	<div class="accordion-heading">
+	  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+	    国土问题
+	  </a>
+	</div>
+	<div id="collapseOne" class="accordion-body in" style="height: auto;">
+        <div class="accordion-inner">
+前一段时间一个段子说，某国的网民在因国土问题与中国网民争吵时说，我要打到北京，中国的网民非常淡然地回应，就你那经济水平，交得起过路费吗？这两天新的段子说，李白要是活在今天的话，估计一大半以上他的诗根本写不出来，因为名山大川的门票他根本买不起。
+        </div>
+	</div>
+</div>    
+
+<script type="text/javascript">
+$('#fat-btn').button('loading')
+
+</script>
 <?php }} ?>
